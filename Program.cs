cf43a6a63f57fb2b105ec93b8d4925ed318e2b43
Golubev Andrey <illidan2936@gmail.com>
2026-09-02 21:11:55 +0300

    Создать проект приложения

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceJournal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Журнал ремонта оборудования.\r\nЗаявка №1: Ремонт принтера - В работе");
+        }
+    }
+}
