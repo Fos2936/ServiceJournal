6dac7d7e56e626f5c8bd8dde7c7d34f45cce78d5
Fos2936 <illidan2936@gmail.com>
2026-09-04 11:48:15 +0300

    Добавить папку для хранения данных зависимостей и вывода данных

---
+++ ServiceJournal.App/Program.cs
@@ -0,0 +1,10 @@
+﻿using ServiceJournal.Logic;
+
+var service = new RepairService();
+
+Console.WriteLine("Отобранные записи:");
+
+foreach (var item in service.GetImportant())
+{
+    Console.WriteLine($"{item.Id}: {item.IsDone}");
+}
